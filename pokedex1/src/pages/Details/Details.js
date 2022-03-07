ba5788bffd0c20d936f19adc9cf23c19c318e913
Ana <a.sammi1103@gmail.com>
2022-03-07 17:36:37 -0300

    leve arrumada no layout da página de detalhes, só pra ver onde cada coisa vai ficar

--- pokedex1/src/pages/Details/Details.js
+++ pokedex1/src/pages/Details/Details.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Container, NameContainer, Images, StatsContainer, TypeContainer } from './styled'
 
 const Details = () => {
+
+
     return (
-      <div>
-        Detalhes do pokemonzinho
-      </div>
+      <Container>
+        <NameContainer>
+            <h2>Nome</h2>
+            <h4>#número</h4>
+        </NameContainer>
+        
+
+        <TypeContainer>
+            <p>tipos</p>
+            <p>moves</p>
+        </TypeContainer>
+
+        <Images>
+            <p>foto do pokemon de frente</p>
+            <p>bundinha do pokemon</p>
+        </Images>
+
+        <StatsContainer>
+            <ul>
+                <li>stats</li>
+                <li>stats</li>
+                <li>stats</li>
+                <li>stats</li>
+                <li>stats</li>
+                <li>stats</li>
+            </ul>
+        </StatsContainer>
+
+      </Container>
     );
   }
   
---
+++ pokedex1/src/pages/Details/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { mainColor, secondColor } from "../../constants/colors";
+
+export const Container = styled.div`
+    display: grid;
+    grid-template: 60px 60px 1fr / 1fr 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    margin-top: 1%;
+`
+
+export const NameContainer = styled.div`
+    color: mainColor;
+    grid-area: 1 / 1 / 2 / 4;
+`
+
+export const TypeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    grid-area: 3 / 1 / 4 / 2
+`
+
+export const Images = styled.div`
+    grid-area: 3 / 2 / 4 / 2;
+`
+
+export const StatsContainer = styled.div`
+    grid-area: 3 / 3 / 4 / 4;
+`
