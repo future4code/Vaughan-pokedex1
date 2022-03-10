8537c26ae3b65cd1692040da85c00c0a9527b8ae
Gabriel Dias Lopes Martins <gdlmartins@Unknown.gateway>
2022-03-10 20:46:23 +0800

    tirando paginacaod o pokedex e fazendo mais uma request e adicionando as no objeto data dentro da variavel global

--- pokedex1/src/global/GlobalState.js
+++ pokedex1/src/global/GlobalState.js
@@ -8,10 +8,13 @@ const GlobalState = (props) => {
     const [offset, setOffset] = useState(0);
     const [add , setAdd] = React.useState([]);
     const [pokemonList, pokemonDetails] = useRequestData([], `${baseURL}/pokemon/?limit=20&offset=${offset}`);
+    const [pokemonListPokedex, pokemonDetailsPokedex] = useRequestData([], `${baseURL}/pokemon/?limit=1120`);
 
     const data = {
         pokemonList,
         pokemonDetails,
+        pokemonListPokedex,
+        pokemonDetailsPokedex,
         currentPage,
         setCurrentPage,
         offset,
